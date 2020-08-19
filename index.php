842e936d19d015cddc0edf6d3045557604c1c7dc
Tirunagari vysali <59056982+Tirunagarivysali@users.noreply.github.com>
2020-08-19 15:18:00 +0530

    Update index.php

--- index.php
+++ index.php
@@ -4,4 +4,5 @@ echo '<form action="mainwebpage.html">';
 echo "<br>";
 echo "Have a look at interesting places to go for  Road trip.";
 echo '<button type="submit">Click to continue</button>';
+echo "</form>";
  ?>
