ea33ab98c7e027e04515ffb8572004b098e4c618
Tirunagari vysali <59056982+Tirunagarivysali@users.noreply.github.com>
2020-08-20 12:23:56 +0530

    Delete index.php

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-echo 'index page';
-echo '<form action="mainwebpage.html">';
-echo "<br>";
-echo "Have a look at interesting places to go for  Road trip.";
-echo '<button type="submit">Click to continue</button>';
-echo "</form>";
- ?>
